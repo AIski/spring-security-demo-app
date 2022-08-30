67ce92fd7db4c42a254c03baf2c79458fd57a5ca
Andrzej Szczepanski <androxtm@gmail.com>
2022-08-30 12:59:55 +0200

    added password stored in database, with encryption!

--- src/main/java/pl/lski/demoapp/DemoappApplication.java
+++ src/main/java/pl/lski/demoapp/DemoappApplication.java
@@ -2,7 +2,6 @@ package pl.lski.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.lski.springsecurity.demo.controller.DemoController;
 
 @SpringBootApplication
 public class DemoappApplication {
--- src/main/java/pl/lski/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/pl/lski/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,44 +1,60 @@
 package pl.lski.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DemoSecurityConfig {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	// add a reference to our security data source
 
-		// add our users for in memory authentication
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE", "MASTER OF THE UNIVERSE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Autowired
+	public DemoSecurityConfig(DataSource theSecurityDataSource) {
+		securityDataSource = theSecurityDataSource;
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/").hasRole("EMPLOYEE")
-				.antMatchers("/leaders/**").hasRole("MANAGER")
-				.antMatchers("/systems/**").hasRole("ADMIN")
-				.and()
-				.formLogin()
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		return new JdbcUserDetailsManager(securityDataSource);
+		}
+
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		return http
+			.authorizeRequests(configurer -> 
+				configurer
+					.antMatchers("/").hasRole("EMPLOYEE")
+					.antMatchers("/leaders/**").hasRole("MANAGER")
+					.antMatchers("/systems/**").hasRole("ADMIN"))
+			
+			.formLogin(configurer -> 
+				configurer
 					.loginPage("/showMyLoginPage")
 					.loginProcessingUrl("/authenticateTheUser")
-					.permitAll()
-				.and()
-				.logout().permitAll()
-				.and()
-				.exceptionHandling().accessDeniedPage("/access-denied");
+					.permitAll())
+			
+			.logout(configurer -> 
+				configurer
+					.permitAll())
+			
+			.exceptionHandling(configurer -> 
+				configurer
+					.accessDeniedPage("/access-denied"))
+			
+			.build();
 	}
 }
